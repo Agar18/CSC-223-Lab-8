cb3ece1ebb9beb2fcd74a11344e174d8bfce6a07
Agar18 <awgardner@me.com>
2023-04-23 15:32:58 -0400

    worked on AngleEquivalenceClasses

--- src/geometry_objects/angle/AngleEquivalenceClasses.java
+++ src/geometry_objects/angle/AngleEquivalenceClasses.java
@@ -1,7 +1,11 @@
 package geometry_objects.angle;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+
 import geometry_objects.angle.comparators.AngleStructureComparator;
 import utilities.eq_classes.EquivalenceClasses;
+import utilities.eq_classes.LinkedEquivalenceClass;
 
 /**
  * Given the figure below:
@@ -20,7 +24,30 @@ import utilities.eq_classes.EquivalenceClasses;
  *   canonical = BAE
  *   rest = BAF, CAE, DAE, CAF, DAF
  */
-public class AngleEquivalenceClasses extends EquivalenceClasses<Angle>
-{
-	// TODO
+public class AngleEquivalenceClasses extends EquivalenceClasses<Angle> {
+
+	public AngleEquivalenceClasses(Comparator<Angle> comparator) {
+		super(new AngleStructureComparator());
+	}
+
+	@Override
+	public boolean add(Angle element) {
+
+		int eqIndex = indexOfClass(element);
+		
+		if(eqIndex != -1)
+		{
+			return _classes.get(eqIndex).add(element);
+		}
+		
+		AngleLinkedEquivalenceClass list = new AngleLinkedEquivalenceClass(_comparator);
+		
+		_classes.add(list);
+		
+		return list.add(element);
+		
+
+
+	}
 }
+
